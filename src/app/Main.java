dd53060d0bbb7e1b66838163e165b1be2d0bab47
KovalenkoBogdan <kbohdan45@gmail.com>
2025-02-03 14:05:02 +0200

    Add kilometr to mile

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,12 +2,19 @@ package app;
 
 public class Main {
     private static double CONV_M = 1.6093;
+    private static double CONV_K = 0.6213711922;
 
     public static void main(String[] args) {
         System.out.println("My SSH app");
-        double ml = 12;
+        double ml = 10;
+        double km = 16;
         double kilometr = convMileToKilomtr(ml);
-        System.out.println("Result is " + kilometr + " kilometriv");
+        double mile = convKilometrToMile(km);
+        System.out.println("Result is " + kilometr + " kilometriv and " + mile + " mile");
+    }
+
+    private static double convKilometrToMile(double km) {
+        return km * CONV_K;
     }
 
     private static double convMileToKilomtr(double ml) {
