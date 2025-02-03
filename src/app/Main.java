3b6acbf49e9bc533f671801a142d38aeffd6b491
KovalenkoBogdan <kbohdan45@gmail.com>
2025-02-03 13:51:10 +0200

    Add mile to kilometr converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,16 @@
 package app;
 
 public class Main {
-    private static double CONV_K = 1.6093;
+    private static double CONV_M = 1.6093;
 
     public static void main(String[] args) {
         System.out.println("My SSH app");
+        double ml = 12;
+        double kilometr = convMileToKilomtr(ml);
+        System.out.println("Result is " + kilometr + " kilometriv");
+    }
+
+    private static double convMileToKilomtr(double ml) {
+        return ml * CONV_M;
     }
 }
